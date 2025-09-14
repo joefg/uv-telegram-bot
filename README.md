bd8cc063f135b34f3af86ea0f9a24ae846a6b408
joefg <joefullergray@gmail.com>
2025-09-14 17:37:16 +0100

    init: Scaffold a Python project with uv

---
+++ README.md
@@ -0,0 +1,3 @@
+# joefg-bot
+
+My personal [Telegram bot](https://core.telegram.org/bots).
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from joefg-bot!")
+
+
+if __name__ == "__main__":
+    main()
